85147f6519ba996d5e85e976f4cf7c035474ec57
David <dockersurface@gmail.com>
2021-02-27 03:20:48 +0800

    --

--- src/api/controller/douyin.js
+++ src/api/controller/douyin.js
@@ -24,10 +24,8 @@ module.exports = class extends Base {
 
   async getAccessTokenAction() {
     const code = this.get('code');
-    console.log(code, 'res')
 
     const res = await axios.get(`https://open.douyin.com/oauth/access_token?client_key=awdh9y4t36b0b4fx&client_secret=d07c7a010d8faab0641a8df70ec1db6e&code=${code}&grant_type=authorization_code`)
-    console.log(res, 'res')
-    return this.success(res.data.data);
+    return this.success(res);
   }
 };
