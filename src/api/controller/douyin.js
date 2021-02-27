40a291db0ab69d4b18085f37e700cdfb111344c9
David <dockersurface@gmail.com>
2021-02-27 12:38:12 +0800

    --

--- src/api/controller/douyin.js
+++ src/api/controller/douyin.js
@@ -37,4 +37,49 @@ module.exports = class extends Base {
     return this.success(res.data.data);
   }
 
+  async getUserInfoAction() {
+    const open_id = this.get('open_id');
+    const access_token = this.get("access_token");
+    const clientIp = this.ctx.ip;
+
+    const res = await axios.get(`https://open.douyin.com/oauth/userinfo?open_id=${open_id}&access_token=${access_token}`)
+    const userInfo = res.data.data;
+    if (think.isEmpty(userInfo)) {
+      return this.fail('登录失败');
+    }
+
+    // 根据openid查找用户是否已经注册
+    let userId = await this.model('user_douyin').where({ open_id: userInfo.open_id }).getField('id', true);
+    if (think.isEmpty(userId)) {
+      // 注册
+      userId = await this.model('user_douyin').add({
+        register_time: parseInt(new Date().getTime() / 1000),
+        register_ip: clientIp,
+        mobile: '',
+        open_id: userInfo.open_id,
+        avatar: userInfo.avatar || '',
+        gender: userInfo.gender || 1, // 性别 0：未知、1：男、2：女
+        nickname: userInfo.nickname,
+        province: userInfo.province,
+        city: userInfo.city,
+        country: userInfo.country,
+        union_id: userInfo.union_id
+      });
+    }
+
+    // 查询用户信息
+    const newUserInfo = await this.model('user_douyin').field(['id', 'nickname', 'gender', 'avatar']).where({ id: userId }).find();
+    // 更新登录信息
+    await this.model('user_douyin').where({ id: userId }).update({
+      last_login_time: parseInt(new Date().getTime() / 1000),
+      last_login_ip: clientIp
+    });
+
+    if (think.isEmpty(newUserInfo)) {
+      return this.fail('登录失败');
+    }
+
+    return this.success(res.data.data);
+  }
+
 };
